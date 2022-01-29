ac21ebf5c36ed08f930fad2d5ddec3fba54c225b
vagrant <wong_michael@rogers.com>
2022-01-29 23:23:56 +0000

    Add addUser query code

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -76,10 +76,29 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`;
+
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+  
+  const values = [name, email, password];
+
+  return pool
+  .query(queryString, values)
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    return null;
+  });
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
